2947d4c2558ac9f0f3ce9b98e86a0ebc32ca46a0
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-03-09 21:13:40 +0100

    Update portfolio-detail.ts

--- api/portfolio-detail.ts
+++ api/portfolio-detail.ts
@@ -1,4 +1,4 @@
-import pool from "./_db";
+import pool from "./_db.js";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
