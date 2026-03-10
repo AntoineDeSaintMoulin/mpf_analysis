cf79993bbe6404a9e214ebd2028d3bce0d45cd7e
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-03-10 17:09:02 +0100

    Create portfolios-list.ts

---
+++ api/api/portfolios-list.ts
@@ -0,0 +1,12 @@
+import pool from "./_db.js";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const portfolios = await pool.query("SELECT * FROM portfolios");
+    res.json(portfolios.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: String(error) });
+  }
+}
