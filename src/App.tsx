eb8c4cc4d7facff93114b1f0600dfe0d5f1e909a
AntoineDeSaintMoulin <91260719+AntoineDeSaintMoulin@users.noreply.github.com>
2026-06-15 16:27:44 +0200

    Refactor debtLeafRows calculation to remove useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -2434,9 +2434,7 @@ let list = debtLeafRows.filter(inst => {
   return list;
 }, [debtData, debtSearch, debtSortConfig]);
  
-const debtLeafRows = React.useMemo(() => 
-  debtData.filter((i: any) => i.level === 4 || (i.level === 5) || (!i.level && i.isin)),
-[debtData]);
+const debtLeafRows = debtData.filter((i: any) => i.level === 4 || i.level === 5 || (!i.level && i.isin));
 const totalDebtWght = debtLeafRows.reduce((s, i) => s + Number(i.wght_pct ?? 0), 0);
 const totalDebtMtm = debtLeafRows.reduce((s, i) => s + Number(i.mtm_ptf ?? 0), 0);
 const avgDuration = debtData.length > 0
